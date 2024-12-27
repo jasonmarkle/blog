7a719e71310c1015a2cd1504762b1339eb9213bc
Jason Markle <jmarkle52@gmail.com>
2024-12-27 10:34:25 -0500

    ugly fix for articles dir

--- lib/blog/articles.ex
+++ lib/blog/articles.ex
@@ -3,22 +3,30 @@ defmodule Blog.Articles do
   Handles loading articles from markdown files
   """
 
-  @articles_dir
-    if Mix.env() == :prod do
-      Path.join([:code.priv_dir(:blog), "../blog-0.1.0", "articles"])
-    else
-      Path.join([:code.priv_dir(:blog), "articles"])
-    end
-
   def list_articles do
-    @articles_dir
+    articles_dir =
+      if Mix.env() == :prod do
+        Path.join([:code.priv_dir(:blog), "../blog-0.1.0", "articles"])
+      else
+        Path.join([:code.priv_dir(:blog), "articles"])
+      end
+    IO.inspect(:code.priv_dir(:blog), label: "Articles Directory")  # Inspect the path
+
+    articles_dir
     |> File.ls!()
     |> Enum.filter(&String.ends_with?(&1, ".md"))
     |> Enum.map(&Path.basename(&1, ".md"))
   end
 
   def get_article(name) do
-    filename = Path.join(@articles_dir, "#{name}.md")
+    articles_dir =
+      if Mix.env() == :prod do
+        Path.join([:code.priv_dir(:blog), "../blog-0.1.0", "articles"])
+      else
+        Path.join([:code.priv_dir(:blog), "articles"])
+      end
+
+    filename = Path.join(articles_dir, "#{name}.md")
 
     if File.exists?(filename) do
       {:ok, content} = File.read(filename)
