75c02e02da494c7310eed024613c60b274eea86d
Jason Markle <jmarkle52@gmail.com>
2024-12-27 09:05:27 -0500

    Articles 0 state

--- lib/blog_web/live/blog_live/articles.ex
+++ lib/blog_web/live/blog_live/articles.ex
@@ -11,17 +11,19 @@ defmodule BlogWeb.BlogLive.Articles do
   end
 
   def render(assigns) do
+    IO.inspect(assigns.articles, label: "articles value")
     ~H"""
     <section class="w-full px-4 py-20 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-6xl">
         <h1 class="text-zinc-100">Blog</h1>
-        <ul class="w-full mx-auto max-w-4xl">
+        <ul :if={@articles != []} class="w-full mx-auto max-w-4xl">
           <li :for={article <- @articles} class="w-full">
             <a class="block w-full line-height-2 text-violet-300 font-bold px-4 py-6 bg-zinc-900 hover:bg-zinc-800 focus:bg-zinc-800 border-solid border-2 border-violet-400 rounded-lg" href={~p"/blog/#{article}"}>
               <%= String.capitalize(article) %>
             </a>
           </li>
         </ul>
+        <p :if={@articles == []} class="text-white">There are no artciles to read yet</p>
       </div>
     </section>
     """
